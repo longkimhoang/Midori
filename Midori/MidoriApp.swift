1567875520c94bb401c19d7e44e7fb987cb110a4
Long Kim Hoang <longkimhoang@outlook.com>
2024-02-26 16:33:38 +0700

    Initial Commit

---
+++ Midori/MidoriApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidoriApp.swift
+//  Midori
+//
+//  Created by Long Kim on 26/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MidoriApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
