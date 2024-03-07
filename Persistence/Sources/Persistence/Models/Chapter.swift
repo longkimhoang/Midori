8a3ac1327af3ae38d537365a1fa6479d1118bc68
Long Kim Hoang <longkimhoang@outlook.com>
2024-03-07 14:12:58 +0700

    Add chapter CoreData model

---
+++ Persistence/Sources/Persistence/Models/Chapter.swift
@@ -0,0 +1,23 @@
+//
+//  Chapter.swift
+//
+//
+//  Created by Long Kim on 07/03/2024.
+//
+
+import CoreData
+import Foundation
+
+@objc(Chapter)
+public final class Chapter: NSManagedObject {
+
+  // MARK: - Attributes
+
+  @NSManaged public var chapterID: UUID
+  @NSManaged public var title: String
+  @NSManaged public var chapter: String
+
+  // MARK: - Relationships
+
+  @NSManaged public var manga: Manga
+}
