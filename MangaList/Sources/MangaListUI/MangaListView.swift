b2ecb864f21594e05df7565071ed6a12cae00ebb
Long Kim Hoang <longkimhoang@outlook.com>
2024-03-15 22:28:20 +0700

    Update layout selection for iOS

--- MangaList/Sources/MangaListUI/MangaListView.swift
+++ MangaList/Sources/MangaListUI/MangaListView.swift
@@ -22,17 +22,35 @@ public struct MangaListView: View {
       .ignoresSafeArea()
       .toolbar {
         ToolbarItem {
-          Picker(selection: $store.layout.sending(\.view.layoutChanged)) {
-            ForEach(MangaListFeature.State.Layout.allCases, id: \.self) {
-              $0.label
+          #if os(iOS)
+          Menu {
+            Section {
+              layoutPicker
+            } header: {
+              Text("Layout", bundle: .module)
             }
           } label: {
-            Text("Layout", bundle: .module)
+            Label("Options", bundle: .module, systemImage: "ellipsis.circle")
           }
-          .pickerStyle(.segmented)
-          .help(Text("Changes the layout of the mangas list.", bundle: .module))
+          .pickerStyle(.inline)
+          #else
+          layoutPicker
+            .pickerStyle(.segmented)
+            .help(Text("Changes the layout of the mangas list.", bundle: .module))
+          #endif
         }
       }
+  }
+
+  @ViewBuilder
+  private var layoutPicker: some View {
+    Picker(selection: $store.layout.sending(\.view.layoutChanged)) {
+      ForEach(MangaListFeature.State.Layout.allCases, id: \.self) {
+        $0.label
+      }
+    } label: {
+      Text("Layout", bundle: .module)
+    }
   }
 }
 
@@ -40,9 +58,9 @@ extension MangaListFeature.State.Layout {
   fileprivate var label: some View {
     switch self {
     case .list:
-      Label("Toggle list view", bundle: .module, systemImage: "list.bullet")
+      Label("List", bundle: .module, systemImage: "list.bullet")
     case .grid:
-      Label("Toggle grid view", bundle: .module, systemImage: "square.grid.2x2")
+      Label("Grid", bundle: .module, systemImage: "square.grid.2x2")
     }
   }
 }
