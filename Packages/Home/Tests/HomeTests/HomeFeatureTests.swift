402df5053f0686b24982d78806064fcd7c3965cc
Long Kim Hoang <longkimhoang@outlook.com>
2024-04-28 18:18:17 +0700

    feat: update tests

--- Packages/Home/Tests/HomeTests/HomeFeatureTests.swift
+++ Packages/Home/Tests/HomeTests/HomeFeatureTests.swift
@@ -15,17 +15,48 @@ final class HomeFeatureTests: XCTestCase {
   func testFetchData() async {
     let uuid = UUIDGenerator.incrementing
     let popularMangas: IdentifiedArrayOf<Manga> = [
-      Manga(id: uuid(), name: "Popular Manga 1", description: "Popular Manga 1"),
-      Manga(id: uuid(), name: "Popular Manga 2", description: "Popular Manga 2"),
+      Manga(
+        id: uuid(),
+        name: "Popular Manga 1",
+        description: "Popular Manga 1",
+        authorName: "Author name 1"
+      ),
+      Manga(
+        id: uuid(),
+        name: "Popular Manga 2",
+        description: "Popular Manga 2",
+        authorName: "Author name 2"
+      ),
+    ]
+    let latestChapters: IdentifiedArrayOf<Chapter> = [
+      Chapter(
+        id: uuid(),
+        title: "Chapter 1",
+        chapter: "1",
+        volume: "1",
+        mangaTitle: "Manga 1",
+        coverImageURL: nil
+      )
     ]
     let recentlyAddedMangas: IdentifiedArrayOf<Manga> = [
-      Manga(id: uuid(), name: "Recent Manga 1", description: "Recent Manga 1"),
-      Manga(id: uuid(), name: "Recent Manga 2", description: "Recent Manga 2"),
+      Manga(
+        id: uuid(),
+        name: "Recent Manga 1",
+        description: "Recent Manga 1",
+        authorName: "Author name 1"
+      ),
+      Manga(
+        id: uuid(),
+        name: "Recent Manga 2",
+        description: "Recent Manga 2",
+        authorName: "Author name 2"
+      ),
     ]
     let store = TestStore(initialState: HomeFeature.State()) {
       HomeFeature()
     } withDependencies: {
       $0.homeData.retrievePopularMangas = { popularMangas }
+      $0.homeData.retrieveLatestChapters = { latestChapters }
       $0.homeData.retrieveRecentlyAddedMangas = { recentlyAddedMangas }
     }
 
@@ -35,6 +66,7 @@ final class HomeFeatureTests: XCTestCase {
       $0.fetchStatus = .success(
         HomeData(
           popularMangas: popularMangas,
+          latestChapters: latestChapters,
           recentlyAddedMangas: recentlyAddedMangas
         )
       )
@@ -52,6 +84,7 @@ final class HomeFeatureTests: XCTestCase {
       $0.homeData.retrievePopularMangas = {
         throw MockError(errorDescription: "Mock")
       }
+      $0.homeData.retrieveLatestChapters = { [] }
       $0.homeData.retrieveRecentlyAddedMangas = { [] }
     }
 
