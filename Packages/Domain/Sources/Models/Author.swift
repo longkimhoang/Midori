52163e052a94841170b30d55d3df3b3a83865603
Long Kim Hoang <longkimhoang@outlook.com>
2024-04-28 16:38:53 +0700

    feat: add author swiftdata model

---
+++ Packages/Domain/Sources/Models/Author.swift
@@ -0,0 +1,30 @@
+//
+//  Author.swift
+//
+//
+//  Created by Long Kim on 28/4/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+public final class Author {
+  public var authorID: UUID
+  public var name: String
+  public var imageURL: URL?
+
+  public init(
+    authorID: UUID,
+    name: String,
+    imageURL: URL? = nil
+  ) {
+    self.authorID = authorID
+    self.name = name
+    self.imageURL = imageURL
+  }
+
+  // MARK: Relationships
+
+  @Relationship public var mangas: [Manga] = []
+}
