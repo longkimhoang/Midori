944d0823d73e5bf53949c1a949a3aa6cedf01e2e
Long Kim Hoang <longkimhoang@outlook.com>
2024-04-29 10:15:59 +0700

    feat: update recently added reducer

--- Packages/Home/Sources/Home/RecentlyAddedFeature.swift
+++ Packages/Home/Sources/Home/RecentlyAddedFeature.swift
@@ -12,6 +12,8 @@ import MangaList
 
 @Reducer
 public struct RecentlyAddedFeature: Sendable {
+  public typealias Manga = MangaList.Manga
+
   @ObservableState
   public struct State: Equatable, Sendable {
     public var mangaList = MangaListFeature.State()
@@ -19,22 +21,34 @@ public struct RecentlyAddedFeature: Sendable {
 
   public enum Action: Sendable {
     case fetchMangas
+    case mangasResponse(Result<IdentifiedArrayOf<Manga>, any Error>, initial: Bool)
     case mangaList(MangaListFeature.Action)
   }
 
-  @Dependency(\.mangaRepository) private var mangaRepository
+  @Dependency(\.recentlyAddedData) private var recentlyAddedData
 
   public var body: some ReducerOf<Self> {
     Scope(state: \.mangaList, action: \.mangaList) {
       MangaListFeature()
     }
 
-    Reduce { _, action in
+    Reduce { state, action in
       switch action {
       case .fetchMangas:
-        .none
+        return .run { send in
+          let initialMangas = try await recentlyAddedData.fetchRecentMangasFromStorage()
+          await send(.mangasResponse(.success(initialMangas), initial: true))
+        }
+      case let .mangasResponse(.success(mangas), initial: true):
+        state.mangaList.mangas = mangas
+        return .none
+      case let .mangasResponse(.success(mangas), initial: false):
+        state.mangaList.mangas.append(contentsOf: mangas)
+        return .none
+      case .mangasResponse(.failure, _):
+        return .none
       case .mangaList:
-        .none
+        return .none
       }
     }
   }
