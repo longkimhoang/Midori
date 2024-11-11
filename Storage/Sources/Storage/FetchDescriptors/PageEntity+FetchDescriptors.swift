676994cefce6ceb730ca9244f39874a32b10eab7
Long Kim Hoang <longkimhoang@outlook.com>
2024-11-12 00:28:08 +0700

    add page fetch descriptor

---
+++ Storage/Sources/Storage/FetchDescriptors/PageEntity+FetchDescriptors.swift
@@ -0,0 +1,21 @@
+//
+//  PageEntity+FetchDescriptors.swift
+//  Storage
+//
+//  Created by Long Kim on 12/11/24.
+//
+
+import Foundation
+import SwiftData
+
+public extension PageEntity {
+    static func withChapterID(_ chapterID: UUID, quality: Quality) -> FetchDescriptor<PageEntity> {
+        var descriptor = FetchDescriptor<PageEntity>()
+        descriptor.predicate = #Predicate {
+            $0.chapter?.id == chapterID && $0.quality == quality
+        }
+        descriptor.sortBy = [.init(\.pageIndex)]
+
+        return descriptor
+    }
+}
