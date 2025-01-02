5ee9a71eb15b3d93ffa4a6e03042c880b98db401
Long Kim Hoang <longkimhoang@outlook.com>
2025-01-02 16:16:20 +0700

    add user feed endpoint

--- MangaDexAPIClient/Sources/MangaDexAPIClient/Endpoint/UserEndpoint.swift
+++ MangaDexAPIClient/Sources/MangaDexAPIClient/Endpoint/UserEndpoint.swift
@@ -5,6 +5,7 @@
 //  Created by Long Kim on 28/12/24.
 //
 
+import Foundation
 import Get
 
 public extension MangaDexAPI {
@@ -20,5 +21,28 @@ public struct GetLoggedInUserResponse: Decodable, Sendable {
 public extension MangaDexAPI.User {
     static func me() -> Request<GetLoggedInUserResponse> {
         Request(path: "/user/me")
+    }
+}
+
+// MARK: - Get user followed manga feed
+
+public struct GetUserFollowedMangaFeedResponse: Decodable, Sendable {
+    public let limit: Int
+    public let offset: Int
+    public let data: [Chapter]
+}
+
+public extension MangaDexAPI.User {
+    static func followedMangaFeed(
+        pagination: Pagination,
+        order: [MangaDexAPI.Chapter.ListSortOptions: SortOrder] = [:],
+        includes: [MangaDexAPI.Chapter.Reference] = [.scanlationGroup]
+    ) -> Request<GetUserFollowedMangaFeedResponse> {
+        var query: [URLQueryItem] = []
+        query.append(contentsOf: pagination.queryItems)
+        query.append(contentsOf: order.queryItems)
+        query.append(contentsOf: includes.queryItems)
+
+        return Request(path: "/user/follows/manga/feed", query: query.map { ($0.name, $0.value) })
     }
 }
